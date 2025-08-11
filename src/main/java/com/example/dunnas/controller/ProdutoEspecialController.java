b54317cdbbf0314174241e46bdc66d7a3839dac6
Antonio Eduardo <antonioeduardooc@gmail.com>
2025-08-11 19:14:41 -0300

    Correção na classe ProdutoEspecialController

--- src/main/java/com/example/dunnas/controller/ProdutoEspecialController.java
+++ src/main/java/com/example/dunnas/controller/ProdutoEspecialController.java
@@ -6,16 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ProdutoViewController {
+public class ProdutoEspecialController {
     @GetMapping("/produtos-fornecedor")
     public String produtosFornecedor(Model model, Authentication authentication) {
-        // Adicione dados ao model se necessário
         return "produtos/produtos-fornecedor";
     }
 
     @GetMapping("/produtos-cliente")
     public String produtosCliente(Model model, Authentication authentication) {
-        // Adicione dados ao model se necessário
         return "produtos/produto-cliente";
     }
 }
